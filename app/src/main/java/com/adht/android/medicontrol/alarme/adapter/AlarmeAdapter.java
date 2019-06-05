9bf4c07bcf924f877bd704e829decb5797641844
dankhael <danilokhael@gmail.com>
2019-06-05 14:07:52 -0300

    Correção e Modificação do Adapter de Alarme

---
+++ app/src/main/java/com/adht/android/medicontrol/alarme/adapter/AlarmeAdapter.java
@@ -0,0 +1,76 @@
+package com.adht.android.medicontrol.alarme.adapter;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.adht.android.medicontrol.R;
+import com.adht.android.medicontrol.alarme.dominio.Alarme;
+import com.adht.android.medicontrol.alarme.persistencia.AlarmeDAOSQLite;
+import com.adht.android.medicontrol.infra.persistencia.DBHelper;
+
+
+import java.util.List;
+
+public class AlarmeAdapter extends RecyclerView.Adapter<AlarmeAdapter.MyViewHolder> {
+
+    private final List<Alarme> listaAlarmes;
+
+
+
+    public AlarmeAdapter(List<Alarme> lista) {
+        this.listaAlarmes = lista;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View itemLista = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_alarme_adapter, parent, false);
+
+        return new MyViewHolder(itemLista);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int position) {
+        Alarme alarme = listaAlarmes.get(position);
+        myViewHolder.nomeRemedio.setText(alarme.getNomeMedicamento());
+        myViewHolder.inicio.setText(alarme.getHorarioInicial().toString());
+        myViewHolder.complemento.setText(alarme.getComplemento());
+        myViewHolder.frequencia.setText(alarme.getFrequenciaHoras());
+        myViewHolder.dias.setText(alarme.getDuracaoDias());
+
+    }
+
+
+
+
+
+    @Override
+    public int getItemCount() {
+        return listaAlarmes.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView nomeRemedio;
+        TextView inicio;
+        TextView frequencia;
+        TextView complemento;
+        TextView dias;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            nomeRemedio = itemView.findViewById(R.id.textRemedio);
+            inicio = itemView.findViewById(R.id.textHora);
+            frequencia = itemView.findViewById(R.id.textFrequencia);
+            complemento = itemView.findViewById(R.id.textComplemento);
+            dias = itemView.findViewById(R.id.textDias);
+        }
+    }
+}
---
+++ app/src/main/java/com/adht/android/medicontrol/alarme/ui/AlarmesListaActivity.java
@@ -0,0 +1,50 @@
+package com.adht.android.medicontrol.alarme.ui;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.adht.android.medicontrol.R;
+import com.adht.android.medicontrol.alarme.adapter.AlarmeAdapter;
+import com.adht.android.medicontrol.alarme.dominio.Alarme;
+import com.adht.android.medicontrol.alarme.persistencia.AlarmeDAOSQLite;
+import com.adht.android.medicontrol.infra.MediControlException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlarmesListaActivity extends AppCompatActivity {
+
+
+    private RecyclerView recyclerViewAlarmes;
+
+    private List<Alarme> listaAlarme = new ArrayList<Alarme>();
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarmes_lista);
+
+        recyclerViewAlarmes = findViewById(R.id.recyclerViewAlarmes);
+
+        //configurar adapter e listando alarmes
+        AlarmeDAOSQLite daoAlarme = new AlarmeDAOSQLite();
+        try {
+            listaAlarme = daoAlarme.listar();
+        } catch (MediControlException e) {
+            e.printStackTrace();
+        }
+        AlarmeAdapter alarmeAdapter = new AlarmeAdapter(listaAlarme);
+
+
+        //configurar recycle view
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerViewAlarmes.setLayoutManager(layoutManager);
+        recyclerViewAlarmes.setHasFixedSize(true);
+        recyclerViewAlarmes.setAdapter(alarmeAdapter);
+
+
+    }
+
+}
